304f2e96a513d0fcbd76a8e293263effecc16835
大河原悠 <solbonta1@gmail.com>
2019-07-21 17:42:00 +0900

    オリジナルアプリ開発3

--- resources/views/admin/app/create.blade.php
+++ resources/views/admin/app/create.blade.php
@@ -3,14 +3,14 @@
 
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成')
+@section('title', 'マイアプリの新規作成')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成</h2>
+                <h2>アプリ新規作成</h2>
             </div>
         </div>
     </div>
