255c77b8f8cc1bc6816f53aec36deb48cf27e672
大河原悠 <solbonta1@gmail.com>
2019-08-06 11:14:30 +0900

    オリジナルアプリ開発17動画作動

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -19,21 +19,14 @@
                             </div>
                             <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
 
-                        <div class="movie col-md-6 mt-4">
+                        <div class="movie col-md-12 mt-4">
                           <tbody>
                               @foreach($posts as $app)
                                   <tr>
-                                    <td>
-                                      <!-- source要素の場合 -->
-                                      <video controls>
-                                      	<!-- source要素が先頭 -->
-                                      	<source src="{{ $headline->movie_path }}.ogv"></source>
-                                      	<source src="{{ $headline->movie_path }}.webm"></source>
-                                      	<source src="{{ $headline->movie_path }}.mp4"></source>
-
+                                    <td>                                      	
+                                      	<video src="{{$headline->movie_path}}" type="video/mp4" controls></video>
                                       	<!-- 要素の最後にフォールバックコンテンツを配置 -->
-                                      	<p>お使いのブラウザでは動画再生に対応していません…。</p>
-                                      </video>
+                                      	<!-- <p>お使いのブラウザでは動画再生に対応していません…。</p> -->
                                     </td>
                                   </tr>
                               @endforeach
@@ -61,7 +54,7 @@
                                 </div>
                             </div>
 
-                            <div class="image col-md-6 text-right mt-4">
+                            <div class="image col-md-12 text-right mt-4">
                                 @if ($post->image_path)
                                     <img src="{{ $post->image_path }}">
                                 @endif
@@ -70,31 +63,8 @@
                               <tbody>
                                 @foreach($posts as $app)
                                     <tr>
-                                        <td><video id="mv-{{$app->id}}" class="col-6" autobuffer>
-                                          <source src="{{$app->image_path}}" type="video/ogg">
-                                            <source src="{{$app->image_path}}" type="video/webm">
-                                              <source src="{{$app->image_path}}" type="video/mp4">
-                                              <p>HTML5に対応していません。</p>
-                                            </video>
-                                            <form>
-                                              <button type ="button" onclick="movplay(0)">再生</button>
-                                              <button type="button" onclick="movplay(1)">一時停止</button>
-                                            </form>
-                                          <script>
-                                            function movplay(num)
-                                            {
-                                              var obj = document.getElementById("mv-{{$app->id}}");
-                                              var n = parseInt(num);
-                                              if ( n == 0 )
-                                              {
-                                                obj.play();
-                                              }
-                                              else
-                                              {
-                                                obj.pause();
-                                              }
-                                            }
-                                          </script>
+                                        <td>
+                                          <video src="{{$app->movie_path}}" type="video/mp4" controls></video>
                                         </td>
                                     </tr>
                                 @endforeach
