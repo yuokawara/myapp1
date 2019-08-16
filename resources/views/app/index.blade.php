5968738151f25d2af0996216178f7a0aff7d372a
大河原悠 <solbonta1@gmail.com>
2019-08-16 14:28:06 +0900

    オリジナルアプリ開発21レイアウト変更

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -1,103 +1,133 @@
 @extends('layouts.front')
 
 @section('content')
+<!-- Navigasion -->
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+	<button class="navbar-toggler" type="button" data-toggle="collapse"
+	data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="#home">Home</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="{{ action('Admin\MyAppController@create') }}">Create</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="{{ action('Admin\MyAppController@index') }}">Edit</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="{{ action('HomeController@index') }}">Register</a>
+			</li>
+		</ul>
+	</div>
+</nav>
+
+<!-- End Navigation -->
 <!-- carousel -->
-    <div id="slides" class="carousel slide" data-ride="carousel">
-      <ul class="carousel-indicators">
-        @foreach ($sliders as $slider)
-	       <li data-target="#slides" data-slide-to="{{$slider->id}}" class="active"></li>
-        @endforeach
-      </ul>
-      <div class="carousel-inner">
-	      <div class="carousel-item active">
-		      <img src="{{ $headline->image_path }}">
-		        <div class="carousel-caption">
-			       <h1 class="display-2">Sample Making2</h1>
-			        <h3>Sample Layout</h3>
-			         <button type="button" class="btn btn-outline-light btn-lg">DEMO</button>
-			         <button type="button" class="btn btn-primary btn-lg">DEMO2</button>
-		        </div>
-	      </div>
-        @foreach ($sliders as $slider)
-        <div class="carousel-item">
-		     <img src="{{ $slider->image_path }}">
-	      </div>
-        @endforeach
+<div id="slides" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    @foreach ($sliders as $slider)
+    <li data-target="#slides" data-slide-to="{{$slider->id}}" class="active"></li>
+    @endforeach
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{ $headline->image_path }}">
+      <div class="carousel-caption">
+        <h1 class="display-2"><span class="font">M</span>y <span class="font">V</span>log <span class="font">C</span>reater</h1>
+
+        <button type="button" class="btn btn-outline-light btn-lg"><span class="font">S</span>tart <span class="font">C</span>reate</button>
       </div>
     </div>
+    @foreach ($sliders as $slider)
+    <div class="carousel-item">
+      <img src="{{ $slider->image_path }}">
+    </div>
+    @endforeach
+  </div>
+</div>
 <!-- carousel end -->
 
-      <div class="container">
-        <hr color="#c0c0c0">
-          @if (!is_null($headline))
-            <div class="row">
-              <!--- Cards -->
-              <div class="container-fluid padding">
-                <div class="row padding">
-	                 <div class="col-md-6">
-		                <div class="card">
-                      @if ($headline->image_path)
-			                 <img class="card-img-top" src="{{ $headline->image_path }}">
-                      @endif
-			                  <div class="card-body">
-				                 <h4>{{ str_limit($headline->title, 70) }}</h4>
-				                  <p class="card-text">{{ str_limit($headline->body, 300) }}</p>
-			                  </div>
-		                </div>
-	                 </div>
-
-	                 <div class="col-md-6">
-		                <div class="card">
-                    @foreach($posts as $app)
-			               <video class="card-img-top" src="{{$headline->movie_path}}" type="video/mp4" controls autoplay muted></video>
-                    @endforeach
-			                <div class="card-body">
-				               <h4>{{ str_limit($headline->title, 70) }}</h4>
-				                <p class="card-text">{{ str_limit($headline->body, 300) }}</p>
-			                </div>
-		                </div>
-	                 </div>
-                </div>
-              </div>
+<div class="container">
+  <hr color="#c0c0c0">
+  @if (!is_null($headline))
+  <div class="row">
+    <!--- Cards -->
+    <div class="container-fluid padding">
+      <div class="row padding">
+        <div class="col-md-6">
+          <div class="card">
+            @if ($headline->image_path)
+            <img class="card-img-top" src="{{ $headline->image_path }}">
+            @endif
+            <div class="card-body">
+              <h4>{{ str_limit($headline->title, 70) }}</h4>
+              <p class="card-text">{{ str_limit($headline->body, 300) }}</p>
             </div>
-          @endif
-      </div>
-
-        <hr color="#c0c0c0">
-
-      <div class="container-fluid padding">
-        @foreach($posts as $post)
-          <div class="row padding">
-	          <div class="col-sm-4">
-		         <div class="card">
-              @if ($post->image_path)
-			         <img class="card-img-top" src="{{ $post->image_path }}">
-			          <div class="card-body">
-				         <h4>{{ str_limit($post->title, 70) }}</h4>
-				         <p class="card-text">{{ str_limit($post->body, 650) }}</p>
-                 <a href="{{ action('Admin\MyAppController@index') }}" class="btn btn-outline-secondary">編集に戻る</a>
-			          </div>
-              @endif
-		         </div>
-	          </div>
-
-	        <div class="col-sm-8">
-		       <div class="card">
-              @if ($post->movie_path)
-			         <video class="card-img-top" src="{{$app->movie_path}}" type="video/mp4" controls autoplay muted></video>
-			        <div class="card-body">
-                <div class="date">
-                  {{ $post->updated_at->format('Y年m月d日') }}
-                </div>
-				       <!-- <h4>{{ str_limit($post->title, 70) }}</h4>
-				       <p class="card-text">{{ str_limit($post->body, 650) }}</p>
-               <a href="#" class="btn btn-outline-secondary">編集に戻る</a> -->
-			        </div>
-              @endif
-		       </div>
-	        </div>
-
           </div>
-        @endforeach
+        </div>
+
+        <div class="col-md-6">
+          <div class="card">
+            @if ($headline->movie_path)
+            <video class="card-img-top" src="{{$headline->movie_path}}" type="video/mp4" controls autoplay muted></video>
+            @endif
+            <div class="card-body">
+              <h4>{{ str_limit($headline->title, 70) }}</h4>
+              <p class="card-text">{{ str_limit($headline->body, 300) }}</p>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+  </div>
+  @endif
+</div>
+
+<hr color="#c0c0c0">
+
+<div class="container">
+  <div class="row">
+    <div class="container-fluid padding">
+      @foreach($posts as $post)
+      <div class="row padding">
+        <div class="col-md-6">
+          <div class="card">
+            @if ($post->image_path)
+            <img class="card-img-top" src="{{ $post->image_path }}">
+            <div class="card-body">
+              <h4>{{ str_limit($post->title, 70) }}</h4>
+              <p class="card-text">{{ str_limit($post->body, 650) }}</p>
+              <a href="{{ action('Admin\MyAppController@index') }}" class="btn btn-outline-secondary">編集に戻る</a>
+            </div>
+            @endif
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="card">
+            @if ($post->movie_path)
+            <video class="card-img-top" src="{{$post->movie_path}}" type="video/mp4" controls autoplay muted></video>
+            <div class="card-body">
+              <div class="date">
+                {{ $post->updated_at->format('Y年m月d日') }}
+              </div>
+              <!-- <h4>{{ str_limit($post->title, 70) }}</h4>
+              <p class="card-text">{{ str_limit($post->body, 650) }}</p>
+              <a href="#" class="btn btn-outline-secondary">編集に戻る</a> -->
+            </div>
+            @endif
+          </div>
+        </div>
+      </div>
+      <hr color="#c0c0c0">
+      @endforeach
+    </div>
+  </div>
+</div>
 @endsection
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -27,6 +27,7 @@
         <link href="{{ asset('css/front.css') }}" rel="stylesheet">
         <!-- オリジナルアプリ用 -->
         <link href="{{ asset('css/index.css') }}" rel="stylesheet">
+        <script src="https://sdk.form.run/js/v2/formrun.js"></script>
     </head>
     <body>
         <div id="app">
@@ -58,24 +59,95 @@
                 {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
                 @yield('content')
             </main>
+            <!-- START CONTACT -->
+<section class="section" id="contact">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <div class="text-center">
+                    <h2><span class="font-weight-bold">Contact</span> Us</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+
+            <div class="col-lg-2">
+                <div class="text-center">
+                    <div>
+
+                    </div>
+                    <div class="mt-3">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- START CONTACT -->
+        <div class="row mt-5">
+          <div class="col-lg-12">
+            <div class="form-kerri">
+              <form class="formrun" action="https://form.run/api/v1/r/jc99edxucstbmc0ixjhd2zeq" method="post">
+                <div class="row">
+                  <div class="col-lg-6">
+                    <div class="form-group mt-2">
+                      <input name="name" id="name" type="text" class="form-control" placeholder="Your Name*" data-formrun-required>
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="form-group mt-2">
+                      <input name="email" id="email" type="email" class="form-control" placeholder="Your Email*" data-formrun-required>
+                      <div data-formrun-show-if-error="メールアドレス">メールアドレスを正しく入力してください</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div class="form-group mt-2">
+                      <input type="text" class="form-control" id="subject" placeholder="Your Subject.." data-formrun-required>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div class="form-group mt-2">
+                      <textarea name="comments" id="comments" rows="4" class="form-control" placeholder="Your message..." data-formrun-required></textarea>
+                      <div data-formrun-show-if-error="お問い合わせ">お問い合わせ入力してください</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-lg-12 text-right mb-3">
+                    <button type="submit" class="btn btn-primary" data-formrun-error-text="未入力の項目があります" data-formrun-submitting-text="送信中...">Submit</button>
+                    <!-- <a href="#" class="btn btn-custom" data-formrun-error-text="未入力の項目があります" data-formrun-submitting-text="送信中...">Submit</a> -->
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- END CONTACT -->
             <footer>
-		<div id="contact" class="offset">
+              <div id="contact" class="offset">
 
-				<div class="row justify-content-center">
-					<div class="col-md-5 text-center">
-						<!-- ここにイメージタグ　-->
-						<p>お問い合わせはこちらから</p>
-						<strong>Contact Info</strong>
-						<p>000-0000-0000<br>email@samplemk.com</p>
+                <div class="row justify-content-center">
+                  <div class="col-md-5 text-center">
+                    <!-- ここにイメージタグ　-->
+                    <p>お問い合わせはこちらから</p>
+                    <strong>Contact Info</strong>
+                    <p>000-0000-0000<br>email@samplemk.com</p>
 
-					</div>
+                  </div>
 
-					<hr class="socket">
-					&copy; YU ORANGE.
+                  <hr class="socket">
+                  &copy; YU ORANGE.
 
-				</div>
-		</div>
-</footer>
+                </div>
+              </div>
+            </footer>
         </div>
     </body>
 </html>
